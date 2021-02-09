89e7d4e6e3da43d83758cb2359cfb3eb00782d09
gjohn96 <johnnymalalis96@gmail.com>
2021-02-09 19:48:27 +0800

    UI 2

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,25 +1,48 @@
 import { extend } from 'jquery'
 import React, { Component } from 'react'
 import Link from 'react-router-dom'
-
-
-export default class Carousel extends Component{
-        render(){
-            return(
-                <div style={{ 
-                    backgroundImage: `url("bg.jpg")`,
-                    height: '79vh',
-                    backgroundSize: 'cover',
-                    backgroundPositionX: 'center'
-                  }}>
+import '../Carousel.css'
+import Carousel from 'react-bootstrap/Carousel' 
 
 
 
+export default class CarouselBootrap extends Component{
+    render() {  
+        return (  
+                <div>  
+                
+                 <div className='' >  
+                 <Carousel>  
+                 <Carousel.Item style={{'height':"500px"}} >  
+                 <img style={{'height':"500px"}}  
+                 className="d-block w-100"  
+                  src={'01.jpg'}  />  
+                   <Carousel.Caption>  
+                     <h3>First Demo </h3>  
+                         </Carousel.Caption>  
+                         </Carousel.Item  >  
+                         <Carousel.Item style={{'height':"500px"}}>  
+                         <img style={{'height':"500px"}}  
+                           className="d-block w-100"  
+                            src={'02.jpg'}    />  
+                               <Carousel.Caption>  
+                           <h3>Second Demo</h3>  
+                              </Carousel.Caption>  
+                                </Carousel.Item>  
+                               <Carousel.Item style={{'height':"500px"}}>  
+                               <img style={{'height':"500px"}}  
+                                className="d-block w-100"  
+                                 src={'bg.jpg'}   />  
+                                <Carousel.Caption>  
+                                 <h3>Third Demo</h3>  
+                                  </Carousel.Caption>  
+                                 </Carousel.Item>  
+                                </Carousel>  
+                        </div>  
+                </div>  
+        )  
+    }  
+}  
+    
 
-                      
 
-                    
-                </div>
-            )
-        }
-}
